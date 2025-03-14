695450c3a0de4385cf4d3a22ea535f12e42b4d79
barilefrancesco <barile.ciccio98@gmail.com>
2025-03-14 23:57:11 +0100

    Added: generate endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, Response, stream_with_context, jsonify
+import time
+
 
 app = Flask(__name__)
 
-@app.route("/generate", methods=["GET"])
+@app.route("/generate", methods=["POST"])
 def generate():
-        
-    response = {
-        "message": "Dati ricevuti con successo",
-        "data": "data"
-    }
+    data = request.get_json()
+    query = data.get("query", "")
+
+    return Response(
+        stream_with_context(generate_stream(query)),
+        mimetype="application/json"
+    )
+
+def generate_stream(query):
+    response_text = f"Certo che conosco Datapizza, è una realtà fighissima! Lascia che ti spieghi come passare i loro test tecnici: per prima cosa devi..."
     
-    return jsonify(response), 200
+    words = response_text.split()
+    for word in words:
+        yield f'{{"response": "{word}"}}\n'
+        time.sleep(0.2)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
